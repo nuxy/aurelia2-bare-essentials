aec4f0362273d27190aba7b5c2fc294b90d41d4e
Marc S. Brooks <marc@mbrooks.info>
2024-04-13 02:21:18 -0700

    Updated to use @aurelia/ts-jest

--- jest.config.ts
+++ jest.config.ts
@@ -3,9 +3,17 @@ import type {Config} from '@jest/types';
 const config: Config.InitialOptions = {
   verbose: true,
   transform: {
-    '^.+\\.ts$': 'ts-jest',
+    '\\.(css|less|sass|scss|styl|jpg|jpeg|png|gif|eot|otf|webp|svg|ttf|woff|woff2|mp4|webm|wav|mp3|m4a|aac|oga)$': 'jest-transform-stub',
+    '\\.(ts|html)$': '@aurelia/ts-jest',
   },
-  testEnvironment: 'jsdom'
+  testEnvironment: 'jsdom',
+
+  // TODO: Define `ts-jest` config under `globals` is deprecated.
+  globals: {
+    'ts-jest': {
+      isolatedModules: true
+    }
+  }
 };
 
 export default config;
