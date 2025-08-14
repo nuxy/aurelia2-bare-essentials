5b5be7857eb9624efea0c3b21117399daceba8fe
Marc S. Brooks <marc@mbrooks.info>
2025-08-14 14:09:47 -0700

    Deprecation alternative (removed in v30.0.0)

--- jest.config.ts
+++ jest.config.ts
@@ -3,17 +3,10 @@ import type {Config} from '@jest/types';
 const config: Config.InitialOptions = {
   verbose: true,
   transform: {
-    '\\.(css|less|sass|scss|styl|jpg|jpeg|png|gif|eot|otf|webp|svg|ttf|woff|woff2|mp4|webm|wav|mp3|m4a|aac|oga)$': 'jest-transform-stub',
-    '\\.(ts|html)$': '@aurelia/ts-jest',
+    '\\.(css|less|sass|scss|styl|jpg|jpeg|png|gif|eot|otf|webp|svg|ttf|woff|woff2|mp4|webm|wav|mp3|m4a|aac|oga)$': ['jest-transform-stub', {}],
+    '\\.(ts|html)$': ['@aurelia/ts-jest', {}],
   },
   testEnvironment: 'jsdom',
-
-  // TODO: Define `ts-jest` config under `globals` is deprecated.
-  globals: {
-    'ts-jest': {
-      isolatedModules: true
-    }
-  }
 };
 
 export default config;
